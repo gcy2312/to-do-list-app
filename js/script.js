96f32a1422d95b9a0522dc5988465a2ce604ec20
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-04-03 14:57:41 -0400

    newitem function

---
+++ js/script.js
@@ -0,0 +1,25 @@
+  if (inputText === ' '){
+    alert("You must write a task!!")
+  }else {
+    $('#list').append(listItem);
+  }
+//Cross out item on list - listen for dblclick
+  function crossOut(){
+    listItem.toggleClass("strike");
+  }
+  listItem.on("dblclick", crossOut)
+
+
+//create delete btn, append to list item, on click -add class "delete"
+  let deleteBtn = $('<deleteButton></deleteButton');
+  deleteBtn.append(document.createTextNode('X'));
+  listItem.append(deleteBtn);
+
+  function deleteListItem(){
+    listItem.addClass("delete");
+  }
+  deleteBtn.on("click",  deleteListItem)
+
+//make ol id:list sortable (reorder)
+  $('#list').sortable();
+}
